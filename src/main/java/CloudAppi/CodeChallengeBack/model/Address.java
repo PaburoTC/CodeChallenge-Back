f7a1f7a79be4ab7ab1ef67cc319c5daea296360a
paburo <pablosky301@gmail.com>
2020-05-16 11:39:58 +0200

    Models created (User && Address)

--- src/main/java/CloudAppi/CodeChallengeBack/model/Address.java
+++ src/main/java/CloudAppi/CodeChallengeBack/model/Address.java
@@ -1,4 +1,91 @@
 package CloudAppi.CodeChallengeBack.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.NonNull;
+import javax.validation.constraints.NotBlank;
+
 public class Address {
+
+    private int id;
+
+    @NotBlank
+    @NonNull
+    private String street;
+
+    @NotBlank
+    @NonNull
+    private String state;
+
+    @NotBlank
+    @NonNull
+    private String city;
+
+    @NotBlank
+    @NonNull
+    private String country;
+
+    @NotBlank
+    @NonNull
+    private String zip;
+
+    public Address(@JsonProperty("street") String street,
+                   @JsonProperty("state") String state,
+                   @JsonProperty("city") String city,
+                   @JsonProperty("country") String country,
+                   @JsonProperty("zip") String zip) {
+        this.street = street;
+        this.state = state;
+        this.city = city;
+        this.country = country;
+        this.zip = zip;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
--- src/main/java/CloudAppi/CodeChallengeBack/model/User.java
+++ src/main/java/CloudAppi/CodeChallengeBack/model/User.java
@@ -1,15 +1,82 @@
 package CloudAppi.CodeChallengeBack.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.NotBlank;
 import java.sql.Date;
 
 public class User {
 
-    public String name;
-    public String email;
 
-    public User(String name, String email){
+    private int id;
+
+    @NotBlank
+    @NonNull
+    private String name;
+
+    @NotBlank
+    @NonNull
+    private String email;
+
+    @NotBlank
+    @NonNull
+    private Date birthdate;
+
+    @NotBlank
+    @NonNull
+    private Address address;
+
+
+    public User(@JsonProperty("name") String name,
+                @JsonProperty("email")String email,
+                @JsonProperty("birthDate")Date birthdate,
+                @JsonProperty("addres")Address address){
         this.name = name;
         this.email = email;
+        this.birthdate = birthdate;
+        this.address = address;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
 
 }
